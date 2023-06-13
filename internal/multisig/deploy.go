5749a86a6078a588f6dc1c79b0aa533ad88c1255
Andrew Harding <aharding@storj.io>
2023-06-12 21:55:52 -0600

    Weaken param requirement for DeployContract

--- internal/multisig/deploy.go
+++ internal/multisig/deploy.go
@@ -5,14 +5,13 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/zeebo/errs"
 
 	"storj.io/briskitall/internal/contract"
 	"storj.io/briskitall/internal/eth"
 )
 
-func DeployContract(opts *bind.TransactOpts, client *ethclient.Client, owners []common.Address, required int64, dailyLimit int64, waiter eth.Waiter) (common.Address, error) {
+func DeployContract(opts *bind.TransactOpts, client bind.ContractBackend, owners []common.Address, required int64, dailyLimit int64, waiter eth.Waiter) (common.Address, error) {
 	contractAddress, contractTx, _, err := contract.DeployMultiSigWalletWithDailyLimit(opts, client, owners, big.NewInt(required), big.NewInt(dailyLimit))
 	if err != nil {
 		return common.Address{}, errs.Wrap(err)
